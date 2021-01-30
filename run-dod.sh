7083d2138c8337556d710ebf1f7d14884e943f65
YOOX\giordanol <luca.giordano@ynap.com>
2021-01-30 10:51:27 +0100

    Fixed dod

--- run-dod.sh
+++ run-dod.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-trap 'docker exec -it docker sh -c "docker-compose down";docker-compose -f ./docker-compose.dod.yml  down' EXIT
-docker-compose -f ./docker-compose.dod.yml  up -d
-sleep 10
+trap 'docker exec -it docker sh -c "docker-compose down --rmi local";docker-compose -f ./docker-compose.dod.yml  down' EXIT
+docker-compose \
+-f ./docker-compose.dod.yml \
+up --build -d
 
+docker logs docker
 docker exec -it docker sh -c "docker-compose up"
 exit 0
